d4634193bcea03d8294581d0142784cb55ef33d0
doomedraven <abrukhovetskyy@google.com>
2025-09-08 09:16:01 +0000

    Update AuraStealer.py

--- cape_parsers/CAPE/core/AuraStealer.py
+++ cape_parsers/CAPE/core/AuraStealer.py
@@ -25,8 +25,8 @@ def parse_blob(data: bytes):
       - Remaining bytes = cipher data of that size
     """
     aes_key, iv, dword1, dword2 = struct.unpack_from(header_format, config_blob, 0)
-    cipher_size = dword1 ^ dword2
-    cipher_data = data[HEADER_SIZE : HEADER_SIZE + ciphertet_size]
+    ciphertext_size = dword1 ^ dword2
+    cipher_data = data[HEADER_SIZE : HEADER_SIZE + ciphertext_size]
     return aes_key, iv, cipher_data
 
 
